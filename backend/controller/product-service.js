24b11edd43a67727f2c3aa1cedff302e10fa7d89
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-05-30 12:45:28 +0100

    crud backend products

--- backend/controller/product-service.js
+++ backend/controller/product-service.js
@@ -7,19 +7,76 @@ const {
 const router = express.Router();
 
 router.post("/", validateAddProduct, isRequestValidated, async (req, res) => {
-  let { title, price, quantity } = req.body;
-  const newProduct = new Products({
-    title,
-    price,
-    quantity,
-  });
-  newProduct.save().then(() => res.json(newProduct));
+  try {
+    let { title, price, quantity } = req.body;
+    const newProduct = new Products({
+      title,
+      price,
+      quantity,
+    });
+    const one = await Products.findOne({ title });
+    if (one) {
+      res.status(403).json({
+        error: true,
+        msg: "Product already exist !",
+      });
+    } else {
+      newProduct.save().then(() => res.json(newProduct));
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: true,
+      msg: "Server error",
+    });
+  }
 });
 
-router.get("/all", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     let data = await Products.find({});
     res.status(200).json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: true,
+      msg: "Server error",
+    });
+  }
+});
+
+router.delete("/", isRequestValidated, async (req, res) => {
+  try {
+    let { id } = req.body;
+    const delted = await Products.deleteOne({ _id: id });
+    res.status(200).json(delted);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: true,
+      msg: "Server error",
+    });
+  }
+});
+
+router.put("/", validateAddProduct, isRequestValidated, async (req, res) => {
+  try {
+    let { id, title, price, quantity } = req.body;
+    const one = await Products.findOne({ title });
+    if (one) {
+      res.status(403).json({
+        error: true,
+        msg: "Product already exist !",
+      });
+    } else {
+      const updated = await Products.findByIdAndUpdate(
+        id,
+        { $set: req.body },
+        { new: true }
+      );
+      let data = await Products.find({});
+      res.status(200).json(data);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({
